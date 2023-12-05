2e82b6887322285cf4efd2a00e6086c41aa383b7
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-05 20:58:11 +0100

    chore: remove auto-generated files

--- source/tml.cpp
+++
@@ -1,8 +0,0 @@
-#include "tml/tml.hpp"
-
-#include <fmt/core.h>
-#include <string>
-
-exported_class::exported_class() : m_name{fmt::format("{}", "tml")} {}
-
-auto exported_class::name() const -> char const* { return m_name.c_str(); }
