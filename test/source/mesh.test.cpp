461fd60fccff0b43649a66b51d46c814ebd2c4e5
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-05 21:50:58 +0100

    feat: add more unit tests

--- test/source/mesh.test.cpp
+++ test/source/mesh.test.cpp
@@ -21,4 +21,33 @@ TEST_CASE("Meshes tests", "[library]")
         REQUIRE(mesh.get_vertices().size() == 8UL);
         REQUIRE(mesh.get_faces().size() == 12UL);
     }
+
+    SECTION("Save a mesh to a PLY file that already exists")
+    {
+        tml::mesh const mesh{"input.ply"};
+        
+        WHEN("Overwriting is allowed")
+        {
+            REQUIRE(mesh.save("output.ply", true) == tml::error_code::none);
+        }
+
+        WHEN("Overwriting is not allowed")
+        {
+            REQUIRE(mesh.save("output.ply", false) == tml::error_code::file_already_exists);
+        }
+    }
+
+    SECTION("Check the adjacent vertices of a vertex")
+    {
+        tml::mesh const mesh{"input.ply"};
+        auto const& vertices = mesh.get_vertices();
+        REQUIRE(vertices[0].adjacents().size() == 5UL);
+        REQUIRE(vertices[1].adjacents().size() == 4UL);
+        REQUIRE(vertices[2].adjacents().size() == 4UL);
+        REQUIRE(vertices[3].adjacents().size() == 5UL);
+        REQUIRE(vertices[4].adjacents().size() == 4UL);
+        REQUIRE(vertices[5].adjacents().size() == 5UL);
+        REQUIRE(vertices[6].adjacents().size() == 5UL);
+        REQUIRE(vertices[7].adjacents().size() == 4UL);
+    }
 }
