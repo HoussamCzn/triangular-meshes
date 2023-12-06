e7f3671601d7bbab1c6a222df3c8d9da8229a90f
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-06 01:12:52 +0100

    imp: add more unit tests for vec3 operators

--- test/source/vec3.test.cpp
+++ test/source/vec3.test.cpp
@@ -27,4 +27,44 @@ TEST_CASE("Vec3 tests", "[library]")
         REQUIRE(vec3.y() == 6.0F);
         REQUIRE(vec3.z() == -3.0F);
     }
+
+    SECTION("Successfully add 2 vec3")
+    {
+        tml::vec3 const vec1{0.0F, 1.0F, 2.0F};
+        tml::vec3 const vec2{3.0F, 4.0F, 5.0F};
+        tml::vec3 const vec3{vec1 + vec2};
+        REQUIRE(vec3.x() == 3.0F);
+        REQUIRE(vec3.y() == 5.0F);
+        REQUIRE(vec3.z() == 7.0F);
+    }
+
+    SECTION("Successfully subtract 2 vec3")
+    {
+        tml::vec3 const vec1{0.0F, 1.0F, 2.0F};
+        tml::vec3 const vec2{3.0F, 4.0F, 5.0F};
+        tml::vec3 const vec3{vec1 - vec2};
+        REQUIRE(vec3.x() == -3.0F);
+        REQUIRE(vec3.y() == -3.0F);
+        REQUIRE(vec3.z() == -3.0F);
+    }
+
+    SECTION("Successfully add a vec3 to another vec3")
+    {
+        tml::vec3 vec1{0.0F, 1.0F, 2.0F};
+        tml::vec3 const vec2{3.0F, 4.0F, 5.0F};
+        vec1 += vec2;
+        REQUIRE(vec1.x() == 3.0F);
+        REQUIRE(vec1.y() == 5.0F);
+        REQUIRE(vec1.z() == 7.0F);
+    }
+
+    SECTION("Successfully subtract a vec3 from another vec3")
+    {
+        tml::vec3 vec1{0.0F, 1.0F, 2.0F};
+        tml::vec3 const vec2{3.0F, 4.0F, 5.0F};
+        vec1 -= vec2;
+        REQUIRE(vec1.x() == -3.0F);
+        REQUIRE(vec1.y() == -3.0F);
+        REQUIRE(vec1.z() == -3.0F);
+    }
 }
